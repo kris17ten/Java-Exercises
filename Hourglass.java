74a97758453ce6b65e7b480c0ea106be411132aa
Kristen Rebello <42438056+kris17ten@users.noreply.github.com>
2018-11-22 20:48:22 +0000

    Update Hourglass.java

--- Hourglass.java
+++ Hourglass.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package csd3202_lab2;
-
 import java.util.Arrays;
 
 /**
  *
  * @author Kristen
  */
-public class CSD3202_Lab2 {
+public class Hourglass {
     /**
      * @param args the command line arguments
      */
